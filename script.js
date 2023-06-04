167bd5dc2679d42095031fd9db8eb58a6c1b5ac4
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-06-04 15:03:54 -0600

    First Commit
    Boilerplate html, css and javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add an interactive feature
+const header = document.querySelector('header');
+header.addEventListener('click', () => {
+  header.style.backgroundColor = '#ff0000';
+  header.style.color = '#fff';
+});
