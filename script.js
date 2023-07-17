dd045bab9ad7600fa5486340fcddfd1de5958934
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-07-17 18:54:06 -0400

    added menu for services section

--- script.js
+++ script.js
@@ -5,27 +5,69 @@ header.addEventListener('click', () => {
   header.style.color = '#fff';
 });
 
-// Get the current year
-var currentYear = new Date().getFullYear();
-
-// Update the year in the footer
-var yearElement = document.getElementById("year");
-if (yearElement) {
-  yearElement.textContent = currentYear;
-}
-
 // Sticky Navbar
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('sticky', window.scrollY > 0);
-  });
-  
-//Hamburger Menu
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', function() {
-  menu.classList.toggle('active');
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('sticky', window.scrollY > 0);
 });
 
+// Hamburger Menu
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburger.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+});
+
+
+
+
+
+
+
+// Services Menu
+document.addEventListener('DOMContentLoaded', function () {
+  var tabMenus = document.querySelectorAll('.tab-menu');
+  var tabContents = document.querySelectorAll('.tab-content');
+
+  function deactivateTabMenus() {
+    tabMenus.forEach(function (menu) {
+      menu.classList.remove('active', 'animate-underline');
+    });
+  }
+
+  function hideAllTabContents() {
+    tabContents.forEach(function (content) {
+      content.style.display = 'none';
+    });
+  }
+
+  function showTabContent(contentId) {
+    const content = document.getElementById(contentId);
+    if (content) {
+      content.style.display = 'block';
+    }
+  }
+
+  function handleClickEvent(event) {
+    const clickedMenu = event.currentTarget;
+    const contentId = 'content-' + clickedMenu.id;
+
+    deactivateTabMenus();
+    hideAllTabContents();
+    clickedMenu.classList.add('active', 'animate-underline');
+    showTabContent(contentId);
+  }
+
+  tabMenus.forEach(function (tabMenu) {
+    tabMenu.addEventListener('click', handleClickEvent);
+  });
+
+  const firstTabMenu = tabMenus[0];
+  const firstContentId = 'content-' + firstTabMenu.id;
+  firstTabMenu.classList.add('active', 'animate-underline');
+  showTabContent(firstContentId);
+});
 
