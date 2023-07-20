f5de46233ecc6ac9c30576f43dd432d0b3e2de9a
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-07-20 17:02:55 -0400

    fixed the javascript in the contact section but still using variables and not objects

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-// Add an interactive feature
-const header = document.querySelector('header');
-header.addEventListener('click', () => {
-  header.style.backgroundColor = '#ff0000';
-  header.style.color = '#fff';
-});
+// Contact Section
+let fullName = "";
+let emailAddress = "";
+let emailSubject = "";
+let emailMessage = "";
 
-// Sticky Navbar
-window.addEventListener('scroll', function () {
-  var navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('sticky', window.scrollY > 0);
-});
-
-// Hamburger Menu
 document.addEventListener('DOMContentLoaded', function () {
+  // Add an interactive feature
+  const header = document.querySelector('header');
+  if (header) {
+    header.addEventListener('click', () => {
+      header.style.backgroundColor = '#ff0000';
+      header.style.color = '#fff';
+    });
+  }
+
+  // Sticky Navbar
+  window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+    if (navbar) {
+      navbar.classList.toggle('sticky', window.scrollY > 0);
+    }
+  });
+
+  // Hamburger Menu
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
 
-  hamburger.addEventListener('click', function () {
-    menu.classList.toggle('active');
-  });
-});
+  if (hamburger && menu) {
+    hamburger.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  }
 
-// Services Menu
-document.addEventListener('DOMContentLoaded', function () {
-  var tabMenus = document.querySelectorAll('.tab-menu');
-  var tabContents = document.querySelectorAll('.tab-content');
+  // Services Menu
+  const tabMenus = document.querySelectorAll('.tab-menu');
+  const tabContents = document.querySelectorAll('.tab-content');
 
   function deactivateTabMenus() {
     tabMenus.forEach(function (menu) {
@@ -63,12 +73,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const firstContentId = 'content-' + firstTabMenu.id;
   firstTabMenu.classList.add('active', 'animate-underline');
   showTabContent(firstContentId);
-});
 
-
-
-// Custom Scrollspy with Horizontal Offset
-document.addEventListener('DOMContentLoaded', function () {
+  // Custom Scrollspy with Horizontal Offset
   const offset = 50; // Set the horizontal offset here (adjust as needed)
 
   const spyLinks = document.querySelectorAll('a.nav-link[href^="#services-section"]');
@@ -86,72 +92,53 @@ document.addEventListener('DOMContentLoaded', function () {
   spyLinks.forEach(function (link) {
     link.addEventListener('click', scrollToSection);
   });
-});
 
+  // Contact Section
+  const submitBtn = document.getElementById('submitBtn');
+  if (submitBtn) {
+    submitBtn.addEventListener('click', function () {
+      console.log('Button pressed');
 
+      fullName = document.getElementById('name').value;
+      console.log('Name:', fullName);
 
+      emailAddress = document.getElementById('email').value;
+      console.log('Email:', emailAddress);
 
+      emailSubject = document.getElementById('subject').value;
+      console.log('Subject:', emailSubject);
 
-// Contact Section
+      emailMessage = document.getElementById('message').value;
+      console.log('Message:', emailMessage);
 
-// Define variables to store the name, email, subject, and message
-let fullName = "";
-let emailAddress = "";
-let emailSubject = "";
-let emailMessage = "";
+      // Show the success message
+      document.getElementById('form-message-success').style.display = 'block';
 
-// Add an event listener to the button element
-document.getElementById('submitBtn').addEventListener('click', function () {
-  // Trigger the console log
-  console.log('Button pressed');
+      // Clear the input fields after submitting
+      document.getElementById('name').value = "";
+      document.getElementById('email').value = "";
+      document.getElementById('subject').value = "";
+      document.getElementById('message').value = "";
 
-  // Get the value from the name input field and save it to the variable
-  fullName = document.getElementById('name').value;
-  console.log('Name:', fullName);
-
-  // Get the value from the email input field and save it to the variable
-  emailAddress = document.getElementById('email').value;
-  console.log('Email:', emailAddress);
-
-  // Get the value from the subject input field and save it to the variable
-  emailSubject = document.getElementById('subject').value;
-  console.log('Subject:', emailSubject);
-
-  // Get the value from the message textarea and save it to the variable
-  emailMessage = document.getElementById('message').value;
-  console.log('Message:', emailMessage);
-
-  // Show the success message
-  document.getElementById('form-message-success').style.display = 'block';
-
-  // Clear the input fields after submitting
-  document.getElementById('name').value = "";
-  document.getElementById('email').value = "";
-  document.getElementById('subject').value = "";
-  document.getElementById('message').value = "";
-
-  // Hide the success message after 5 seconds (5000 milliseconds)
-  setTimeout(function () {
-    document.getElementById('form-message-success').style.display = 'none';
-  }, 5000);
-});
-
-window.onbeforeunload = () => {
-  for (const form of document.getElementsByTagName('form')) {
-    form.reset();
+      // Hide the success message after 5 seconds (5000 milliseconds)
+      setTimeout(function () {
+        document.getElementById('form-message-success').style.display = 'none';
+      }, 5000);
+    });
   }
-}
 
+  // ... rest of your code ...
 
+  window.addEventListener('beforeunload', function () {
+    for (const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  });
 
-
-
-
-// Footer
-
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Update the year in the HTML
-const yearElement = document.getElementById('year');
-yearElement.textContent = currentYear;
+  // Footer
+  const currentYear = new Date().getFullYear();
+  const yearElement = document.getElementById('year');
+  if (yearElement) {
+    yearElement.textContent = currentYear;
+  }
+});
